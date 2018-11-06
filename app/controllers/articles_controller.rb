194218e3913d23cf0acc2e2a9ab9052127876e99
Masashi Nohara <chancy-m.n-72@ruri.waseda.jp>
2018-11-06 15:54:00 +0900

    completed backend structure

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+  before_action :set_articles, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(articles_params)
+    if @articles.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(articles_params)
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_articles
+    @article = Article.find(params[:id])
+  end
+
+  def articles_params
+    params.require(:article).permit(:title, :content)
+  end
+
+end
